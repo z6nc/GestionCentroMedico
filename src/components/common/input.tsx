914e7b12f89555846bb5a32f5d29d6ab5eb36646
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-13 18:02:28 -0500

    Refactor Input component to remove Textlabel prop and use children for label content

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -2,20 +2,24 @@ import type { InputHTMLAttributes } from "react";
 import type { UseFormRegisterReturn } from "react-hook-form";
 export  interface PROPSINPUT extends InputHTMLAttributes<HTMLInputElement>{
     id:string,
-    Textlabel : string,
     target?: "_self" | "_blank" ,
     register?: UseFormRegisterReturn,
     error?: string,
     estilo ? :string,
     type: "text" | "email" | "password" | "number" 
+    placeholder?: string
+    children?: React.ReactNode
 }
 
 
-export const Input = ({ id, Textlabel, register, error, estilo, type }: PROPSINPUT) => {
+export const Input = ({ id, register, error, estilo, type, placeholder  , children }: PROPSINPUT) => {
     return (
-        <div className={`${estilo} flex flex-col  gap-y-2 text-black`}>
-            <label htmlFor={id}>{Textlabel}</label>
-            <input className="p-2 h-full border border-gray-500 rounded-lg"   name={id} id={id} type={type} {...register} />
+        <div className={`${estilo} w-96 flex flex-col  gap-y-2 text-black`}>
+             <label htmlFor={id} className="inline-flex items-center justify-center border border-gray-300 rounded-sm px-2">
+                 {children}
+                 <span className="text-gray-300 pl-2">|</span>
+              <input className="px-2 py-3 h-full border-none w-full  outline-none "   name={id} id={id} type={type} placeholder={placeholder} {...register} />
+             </label>
             {error && <span className="text-red-500 text-sm">{error}</span>}
         </div>
     )
