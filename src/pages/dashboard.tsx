18bdc2d732c908ee984deac8da361da77eea37d5
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-13 20:28:40 -0500

    Refactor Dashboard component to integrate Sidebar and enhance layout structure

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,19 @@
 import { useAtom } from "jotai";
 import { UserAtom } from "../store/user.atom";
+import { Sidebar } from "../components/layout/Navbar/navbar";
 export default function Dashboard() {
-    const [user] = useAtom(UserAtom);
+    const [user ] = useAtom(UserAtom);
     return (
-        <div>
-            <h1>Dashboard - Bienvenido {user?.nombre}</h1>
-            <p>Tu correo electrónico es: {user?.tipo}</p>
-        </div>
+        <main className="flex bg-[#f8f8f8] ">
+             {user && <Sidebar user={user} />}
+            <div className=" py-4 px-6 ">
+                <div className="mb-8">
+                    <h1 className="text-3xl font-bold ">Panel de Administración</h1>
+                    <p className="text-gray-400 mt-1 text-lg font-medium">Hospital San Rafael - Dashboard</p>
+                </div>
+                {/* <ListaItemsCategoria categoria={DatosEmpleado.RolEmpleado} /> */}
+
+            </div>
+        </main>
     )
 }
