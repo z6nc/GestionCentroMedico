8fb7c59e349a4c8333efb0f23517b2c9e6871883
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-13 20:28:07 -0500

    Fix formatting and spacing in Login component

--- src/components/layout/Login/login.tsx
+++ src/components/layout/Login/login.tsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '../../common/input';
-import { IconEmail , IconPassword} from '../../icons/FormIcon';
+import { IconEmail, IconPassword } from '../../icons/FormIcon';
 import { Link } from 'react-router-dom';
 import { loginSchema } from '../../../schema/login.schema';
 import type { LoginForm } from '../../../schema/login.schema';
@@ -17,20 +17,20 @@ export default function Login({ onSubmit }: { onSubmit: (data: LoginForm) => voi
                 <img className='w-60 mb-6' src="https://api.centromedicoosi.com/img/osi/logologin.png" alt="" />
                 <h1 className="text-blue-700 font-black ">BIENVENIDO</h1>
                 <h2 className='text-gray-500 font-semibold'>Inicia Sesión</h2>
-        
+
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-9">
-                    <Input  id='email' register={register('email')} error={errors.email?.message} type='email' placeholder='Email' >
+                    <Input id='email' register={register('email')} error={errors.email?.message} type='email' placeholder='Email' >
                         <IconEmail />
                     </Input>
-                    <Input id='password'  register={register('password')} error={errors.password?.message} type='password' placeholder='Contraseña' >
+                    <Input id='password' register={register('password')} error={errors.password?.message} type='password' placeholder='Contraseña' >
                         <IconPassword />
                     </Input>
-                        <button
-                            type="submit"
-                            className="w-full bg-green-600 text-white py-2 rounded hover:bg-blue-700 disabled:opacity-50"
-                        >
-                            Inicia Sesión
-                        </button>
+                    <button
+                        type="submit"
+                        className="w-full bg-green-600 text-white py-2 rounded hover:bg-blue-700 disabled:opacity-50"
+                    >
+                        Inicia Sesión
+                    </button>
                 </form>
 
                 <p className="mt-4 text-sm text-center">
