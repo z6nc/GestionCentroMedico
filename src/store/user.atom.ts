ad0e4a74531a5a152850adf183dc40e84196769e
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-13 20:28:57 -0500

    Update UserAtom to use PropsUser type for improved type safety

--- src/store/user.atom.ts
+++ src/store/user.atom.ts
@@ -1,7 +1,7 @@
 // authAtoms.ts
 import { atom } from 'jotai';
-import type { User } from '../types/user.types';
+import type { PropsUser } from '../types/user.types';
 
-export const UserAtom = atom<User | null>(null);
+export const UserAtom = atom<PropsUser | null>(null);
 
 // También puedes crear un atom derivado para saber si hay usuario logueado
