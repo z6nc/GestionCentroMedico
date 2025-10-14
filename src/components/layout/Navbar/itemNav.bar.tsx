137be0617aad342b46cb0a9fb982394394993ce2
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 17:13:08 -0500

    Refactor ItemNavBar component for improved layout and styling

--- src/components/layout/Navbar/itemNav.bar.tsx
+++ src/components/layout/Navbar/itemNav.bar.tsx
@@ -1,22 +1,22 @@
 import { sidebarItems } from "../../../data/itemsDataNavbar";
 
-export const ItemNavBar = ({ tipo }: {tipo: 'medico' | 'enfermero' | 'administrativo' | 'cajero';}) => {
+export const ItemNavBar = ({ tipo }: { tipo: 'medico' | 'enfermero' | 'administrativo' | 'cajero'; }) => {
     const FindSiderItems = sidebarItems.find((item) => item.Categoria === tipo);
     return (
-        <nav className="space-y-2 flex flex-col items-center">
+        <nav className="space-y-5 flex flex-col items-center text-white">
             {
-                        FindSiderItems ? (
-                            FindSiderItems.itemsLabel.map((item , index) => (
-                                    <button className="flex items-center gap-3 w-full px-4 py-2 rounded-lg bg-sky-300/5 border border-gray-200"
-                                        key={index}
-                                    >
-                                        <item.icon className="h-4 w-4" />
-                                        {item.label}
-                                    </button>
+                FindSiderItems ? (
+                    FindSiderItems.itemsLabel.map((item, index) => (
+                        <button className="flex items-center gap-3 w-full px-4 py-2 rounded-lg hover:bg-white/20 transition-colors text-left"
+                            key={index}
+                        >
+                            <item.icon className="size-5 " />
+                            {item.label}
+                        </button>
 
-                            ))
-                        ) : "no hay "
-                    }
+                    ))
+                ) : "no hay "
+            }
 
         </nav>
     )
