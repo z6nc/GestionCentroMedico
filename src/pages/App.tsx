5dedb9828a29e68a3fd183721a552a0ff3371654
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:23 -0500

    Refactor App component to improve routing structure and enhance readability

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,18 +2,41 @@ import { Routes, Route } from "react-router-dom";
 import { ProteccionRouter } from "../auth/ProteccionRouter";
 import VistaLogin from "./vistaLogin";
 import Dashboard from "./dashboard";
+import { sidebarItems } from "../data/itemsDataNavbar";
+import { VistaPrincipal } from "../components/layout/Dasboard/vistaPrincipal";
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<VistaLogin />} />
-      <Route path="/dashboard" element={
-        <ProteccionRouter>
-          <Dashboard />
-        </ProteccionRouter>
-      }
-      />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProteccionRouter>
+            <Dashboard />
+          </ProteccionRouter>
+        }
+      >
+        <Route index element={<VistaPrincipal />} />
+        {/* Subrutas del dashboard */}
+        {sidebarItems.map(routes => (
+          routes.itemsLabel.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <ProteccionRouter allowedRoles={route.allowedRoles}>
+                  {route.element}
+                </ProteccionRouter>
+              }
+            />
+          ))
+        ))}
+
+      </Route>
     </Routes>
+
   )
 }
 
