fd236a1ddc9581b8c9f2112b92cdc7bcf1cc5944
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:06 -0500

    Refactor ItemNavBar to use Link component for navigation and improve type definition

--- src/components/layout/Navbar/itemNav.bar.tsx
+++ src/components/layout/Navbar/itemNav.bar.tsx
@@ -1,18 +1,25 @@
 import { sidebarItems } from "../../../data/itemsDataNavbar";
+import { Link, useLocation } from "react-router-dom";
+import type { Role } from "../../../types/roles.types";
 
-export const ItemNavBar = ({ tipo }: { tipo: 'medico' | 'enfermero' | 'administrativo' | 'cajero'; }) => {
+export const ItemNavBar = ({ tipo }: { tipo: Role }) => {
+    const { pathname } = useLocation();
     const FindSiderItems = sidebarItems.find((item) => item.Categoria === tipo);
+    const linkClass = (path: string) =>
+        ` ${pathname === path ? "bg-white/20 " : "bg-transparent hover:bg-white/20"
+        }`;
     return (
+
         <nav className="space-y-5 flex flex-col items-center text-white">
             {
                 FindSiderItems ? (
                     FindSiderItems.itemsLabel.map((item, index) => (
-                        <button className="flex items-center gap-3 w-full px-4 py-2 rounded-lg hover:bg-white/20 transition-colors text-left"
+                        <Link to={item.path} className={`flex items-center gap-3 w-full px-4 py-2 rounded-lg  transition-colors text-left ${linkClass(item.path)}`}
                             key={index}
                         >
                             <item.icon className="size-5 " />
                             {item.label}
-                        </button>
+                        </Link>
 
                     ))
                 ) : "no hay "
