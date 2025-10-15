68aabb4b159af9602f328c0a79289f6ef0ca3728
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:11:50 -0500

    Enhance ProteccionRouter to support role-based access control

--- src/auth/ProteccionRouter.tsx
+++ src/auth/ProteccionRouter.tsx
@@ -4,12 +4,18 @@ import { UserAtom } from "../store/user.atom";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
+  allowedRoles?: string[];
 }
-export const ProteccionRouter = ({ children }: ProtectedRouteProps) => {
+
+export const ProteccionRouter = ({ children, allowedRoles }: ProtectedRouteProps) => {
   const [user] = useAtom(UserAtom);
 
-  if ( !user) {
+  if (!user) {
     return <Navigate to="/" replace />;
+  }
+  // Si la ruta tiene restricción de roles
+  if (allowedRoles && !allowedRoles.includes(user.tipo)) {
+    return <Navigate to="/dashboard" replace />; // O puedes enviar a "no autorizado"
   }
 
   return <>{children}</>;
