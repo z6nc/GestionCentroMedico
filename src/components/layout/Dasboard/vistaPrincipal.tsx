8b97168020c899fb91369ee25d33339ac812a949
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:01 -0500

    Add VistaPrincipal component for main dashboard layout

---
+++ src/components/layout/Dasboard/vistaPrincipal.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "../../icons/Logo"
+export const VistaPrincipal = () => {
+    return (
+        <section className=" h-full flex flex-col justify-center items-center gap-6">
+                <Logo iconClass="size-90 text-gray-200" />
+
+        </section>
+    )
+}
