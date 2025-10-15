bc7ee308916086c5ccca42b367de9831f023c338
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:27 -0500

    Refactor Dashboard component layout for improved structure and readability

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,19 +1,15 @@
 import { useAtom } from "jotai";
 import { UserAtom } from "../store/user.atom";
 import { Sidebar } from "../components/layout/Navbar/navbar";
+import { Outlet } from "react-router-dom";
 export default function Dashboard() {
-    const [user ] = useAtom(UserAtom);
+    const [user] = useAtom(UserAtom);
     return (
-        <main className="flex bg-[#f8f8f8] ">
-             {user && <Sidebar user={user} />}
-            <div className=" py-4 px-6 ">
-                <div className="mb-8">
-                    <h1 className="text-3xl font-bold ">Panel de Administración</h1>
-                    <p className="text-gray-400 mt-1 text-lg font-medium">Hospital San Rafael - Dashboard</p>
-                </div>
-                {/* <ListaItemsCategoria categoria={DatosEmpleado.RolEmpleado} /> */}
-
-            </div>
-        </main>
+        <div className="flex bg-[#f8f8f8] ">
+            {user && <Sidebar user={user} />}
+            <main className="flex-1  overflow-y-auto">
+                <Outlet />
+            </main>
+        </div>
     )
 }
