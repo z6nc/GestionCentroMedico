3ba31099d8561ea5c527ae0a437734eb0678564d
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:31 -0500

    Add index file to export admin view components for better organization

---
+++ src/pages/VistaAdmin/index.ts
@@ -0,0 +1,6 @@
+export * from "./vistaCitas";
+export * from "./vistaPacientes";
+export * from "./vistaHistoriaMedica";
+export * from "./vistaMedicos";
+export * from "./vistaMedicamentos";
+export * from "./vistaAnalisis";
