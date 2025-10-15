a652b15e84575c660b2ad806a9ae176105d04d19
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:11:58 -0500

    Add TituloCustom component for patient management header

---
+++ src/components/common/titulos/tituloCustom.tsx
@@ -0,0 +1,7 @@
+export const TituloCustom = () => {
+    return (
+        <div className=" border-b border-gray-100 bg-white p-6">
+            <h1 className="text-2xl font-bold text-gray-700">Gestion de Pacientes</h1>
+        </div>
+    )
+}
