00984a151610a5810eb080a2a9d73b66a929d16e
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-14 21:14:16 -0500

    Refactor sidebarItems to include paths and elements for navigation, removing unused Home icon

--- src/data/itemsDataNavbar.tsx
+++ src/data/itemsDataNavbar.tsx
@@ -1,43 +1,35 @@
-import { Users, Calendar, Home, Stethoscope, Pill , CalendarSearch } from "lucide-react"
+import { Users, Calendar, Stethoscope, Pill, CalendarSearch } from "lucide-react"
+import { VistaCitas, VistaPacientes, VistaHistoriaMedica, VistaMedicos, VistaMedicamentos, VistaAnalisis } from "../pages/VistaAdmin/index"
+import { VerBoletas } from "../pages/VistaCajero/index";
+import { VistaAtencionMedica } from "../pages/VistaMedico";
+import type { SidebarItem } from "../types/siderBarItem.types";
 
-interface SidebarItem {
-    Categoria: "administrativo" | "cajero" | "medico";
-    itemsLabel: {
-        icon: React.ComponentType<{ className?: string }>
-        label: string
-        active?: boolean
-
-    }[]
-}
 
 export const sidebarItems: SidebarItem[] = [
     {
         Categoria: "administrativo",
         itemsLabel: [
-            { icon: Home, label: "Inicio", active: true },
-            { icon: Users, label: "Pacientes" },
-            { icon: Calendar, label: "Citas" },
-            { icon: Calendar, label: "Historia Medica" },
-            { icon: Stethoscope, label: "Doctores" },
-            { icon: Pill, label: "Farmacia" },
-            { icon: Pill, label: "Analisis" },
+            { icon: Users, label: "Pacientes", path: "/dashboard/pacientes", element: <VistaPacientes />, allowedRoles: ["administrativo"] },
+            { icon: Calendar, label: "Citas", path: "/dashboard/citas", element: <VistaCitas />, allowedRoles: ["administrativo"] },
+            { icon: Calendar, label: "Historia Medica", path: "/dashboard/historia-medica", element: <VistaHistoriaMedica />, allowedRoles: ["administrativo"] },
+            { icon: Stethoscope, label: "Lista Medicos", path: "/dashboard/lista-medicos", element: <VistaMedicos />, allowedRoles: ["administrativo"] },
+            { icon: Pill, label: "Medicamentos", path: "/dashboard/lista-medicamentos", element: <VistaMedicamentos />, allowedRoles: ["administrativo", "cajero"] },
+            { icon: Pill, label: "Analisis", path: "/dashboard/analisis", element: <VistaAnalisis />, allowedRoles: ["administrativo"] },
 
         ]
     },
-     {
+    {
         Categoria: "cajero",
         itemsLabel: [
-            { icon: Home, label: "Inicio", active: true },
-            { icon: CalendarSearch, label: "Boleta" },
-            { icon: Pill, label: "Listar Medicamentos" },
+            { icon: CalendarSearch, label: "Boleta", path: "/dashboard/boleta", element: <VerBoletas />, allowedRoles: ["cajero", "medico"] },
+            { icon: Pill, label: "Listar Medicamentos", path: "/dashboard/lista-medicamentos", allowedRoles: ["cajero", "administrativo"], element: <VistaMedicamentos /> },
         ]
     },
-       {
+    {
         Categoria: "medico",
         itemsLabel: [
-            { icon: Home, label: "Inicio", active: true },
-            { icon: CalendarSearch, label: "Pagos" },
-            { icon: Pill, label: "Atencion Medica" },
+            { icon: CalendarSearch, label: "Boleta de Pago", path: "/dashboard/boleta", element: <VerBoletas />, allowedRoles: ["medico", "cajero"] },
+            { icon: Pill, label: "Atencion Medica", path: "/dashboard/atencion-medica", element: <VistaAtencionMedica />, allowedRoles: ["medico"] },
 
         ]
     }
