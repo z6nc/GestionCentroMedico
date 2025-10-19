e6a5ac1b6b893bacc85729081a0a69066063e7d8
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-19 13:55:37 -0500

    Update TituloCustom component styles for improved layout and appearance

--- src/components/common/titulos/tituloCustom.tsx
+++ src/components/common/titulos/tituloCustom.tsx
@@ -1,6 +1,6 @@
 export const TituloCustom = () => {
     return (
-        <div className=" border-b border-gray-100 bg-white p-6">
+        <div className=" border-b border-gray-100 bg-white py-7 px-5 shadow-sm">
             <h1 className="text-2xl font-bold text-gray-700">Gestion de Pacientes</h1>
         </div>
     )
