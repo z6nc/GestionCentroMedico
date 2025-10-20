1ab618bc153c57ca7468f77346fd504f46562f61
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-20 13:24:11 -0500

    Refactor VistaPacientes component to include AgregarPacientes and VistaCRUD for improved patient management

--- src/pages/VistaAdmin/vistaPacientes.tsx
+++ src/pages/VistaAdmin/vistaPacientes.tsx
@@ -1,10 +1,19 @@
 import { TituloCustom } from "../../components/common/titulos/tituloCustom"
-import { ListaPacientes } from "../../components/layout/Pacientes/ListaPacientes"
-export  function VistaPacientes() {
+import { VistaCRUD } from "../../components/common/Tablas/vistaCrud"
+import { PersonaData } from "../../data/pacientes.data"
+import { AgregarPacientes } from "../../components/layout/Pacientes/AgregarPacientes"
+export function VistaPacientes() {
     return (
-         <section className="">
-            <TituloCustom />
-            <ListaPacientes />
-         </section>
+        <section className="">
+            <TituloCustom titulo="Pacientes" />
+            <div className="max-w-max mx-auto my-9 flex flex-col gap-y-9 items-start">
+                <AgregarPacientes titulo="Paciente" />
+                <VistaCRUD
+                    data={PersonaData}
+                />
+
+            </div>
+
+        </section>
     )
 }
