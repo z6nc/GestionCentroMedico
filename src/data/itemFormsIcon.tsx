a319c7c8b11b563114e4b40eef28e543b6ee8433
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-20 17:18:45 -0500

    Add ItemFormsIcon component for consistent icon usage in forms

---
+++ src/data/itemFormsIcon.tsx
@@ -0,0 +1,10 @@
+import { IdCard, User ,  CalendarSearch ,VenusAndMars, Phone, MapPinHouse , Mails } from "lucide-react"
+export const ItemFormsIcon = {
+   dni : <IdCard />,
+   usuario : <User />,
+   calendario : <CalendarSearch />,
+   genero : <VenusAndMars />,
+   telefono : <Phone />,
+   direccionHogar : <MapPinHouse />,
+   email: <Mails />
+}
