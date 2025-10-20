e36e8061636d8a4e1f720066de858ee12828d04e
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-20 17:18:28 -0500

    Enhance TablaCustom component to support editing with a button and improve layout

--- src/components/common/Tablas/tabla.tsx
+++ src/components/common/Tablas/tabla.tsx
@@ -2,23 +2,25 @@ import { Pencil } from "lucide-react"
 
 export type TablaCustomProps<T> = {
     datas: T[];
+    onEdit: (item: T) => void; 
 };
 
-export const TablaCustom = <T extends Record<string, unknown>>({ datas }: TablaCustomProps<T>) => {
+export const TablaCustom = <T extends Record<string, unknown>>({ datas, onEdit }: TablaCustomProps<T>) => {
+    const keys = Object.keys(datas[0]) as (keyof T)[];
 
+    // Manejo de caso cuando no hay datos
     if (!datas || datas.length === 0) {
         return <p className="text-center p-4">No hay datos para mostrar</p>;
     }
-    const keys = Object.keys(datas[0]) as (keyof T)[];
-    
+
     return (
-        <div className="relative overflow-x-auto shadow-md rounded-lg max-w-max mx-auto  ">
+        <div className="relative shadow-md rounded-lg  overflow-x-scroll max-w-5xl  2xl:max-w-max">
             <table className="w-full text-sm   ">
                 <thead className="text-xs text-gray-800  uppercase bg-green-400">
                     <tr>
                         {keys.map((key) => (
                             <th key={String(key)}
-                             scope="col" className="px-9 py-5">
+                                scope="col" className="px-9 py-5">
                                 {String(key)}
                             </th>
                         ))}
@@ -44,15 +46,15 @@ export const TablaCustom = <T extends Record<string, unknown>>({ datas }: TablaC
                                 </td>
                             ))}
 
-                            <td className="px-9 py-4 text-center ">
-                                <a
-                                    href="#"
-                                    className=" "
+                            <td className="px-9 py-4 text-center">
+                                <button
+                                    type="button"
+                                    onClick={() => onEdit(item)}
+                                    className="bg-orange-200 hover:bg-orange-300 transition flex items-center justify-center py-2 rounded-lg w-10 mx-auto"
+                                    title="Editar registro"
                                 >
-                                    <div className="bg-orange-200 flex flex-col items-center py-2 rounded-lg ">
-                                        <Pencil className=" size-5  rounded-lg text-black" />
-                                    </div>
-                                </a>
+                                    <Pencil className="size-5 text-black" />
+                                </button>
                             </td>
                         </tr>
                     ))}
