530af29012286c6250d8d84e520ae08a9795e662
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-20 13:23:42 -0500

    Refactor TituloCustom component to accept dynamic title prop for improved flexibility

--- src/components/common/titulos/tituloCustom.tsx
+++ src/components/common/titulos/tituloCustom.tsx
@@ -1,7 +1,7 @@
-export const TituloCustom = () => {
+export const TituloCustom = ({titulo}: {titulo: string}) => {
     return (
         <div className=" border-b border-gray-100 bg-white py-7 px-5 shadow-sm">
-            <h1 className="text-2xl font-bold text-gray-700">Gestion de Pacientes</h1>
+            <h1 className="text-2xl font-bold text-gray-700">{`Gestion de ${titulo}`}</h1>
         </div>
     )
 }
