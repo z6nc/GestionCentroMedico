1ad87956a5235a1740fcabe1f86be36a85389862
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-21 10:55:20 -0500

    Refactor VistaPacientes to use VistaCRUDPaciente component for patient management

--- src/pages/VistaAdmin/vistaPacientes.tsx
+++ src/pages/VistaAdmin/vistaPacientes.tsx
@@ -1,5 +1,5 @@
 import { TituloCustom } from "../../components/common/titulos/tituloCustom"
-import { VistaCRUD } from "../../components/common/Tablas/vistaCrud"
+import { VistaCRUDPaciente } from "../../components/layout/Pacientes/vistaCrud"
 import { PersonaData } from "../../data/pacientes.data"
 import { AgregarPacientes } from "../../components/layout/Pacientes/AgregarPacientes"
 export function VistaPacientes() {
@@ -8,15 +8,14 @@ export function VistaPacientes() {
             <TituloCustom titulo="Pacientes" />
             <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
                 <AgregarPacientes titulo="Paciente" />
-                
-                <VistaCRUD
+
+                <VistaCRUDPaciente
                     data={PersonaData}
                     titulo="pacientes"
                     onEdit={(actualizado) => {
                         console.log("Paciente actualizado:", actualizado);
                     }}
                 />
-
             </div>
 
         </section>
