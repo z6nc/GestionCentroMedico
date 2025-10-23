dc9b7ed165e1313575647df12f95b2a6c170742e
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-23 17:26:54 -0500

    Add VistaAnalisis component for analysis management with CRUD functionality

--- src/pages/VistaAdmin/vistaAnalisis.tsx
+++ src/pages/VistaAdmin/vistaAnalisis.tsx
@@ -1,5 +1,23 @@
+import { TituloCustom } from "../../components/common/titulos/tituloCustom"
+import { VistaCRUDAnalisis } from "../../components/layout/Analisis/vistaCrudAnalisis"
+import { AnalisisData } from "../../data/analisis.data"
+import { AgregarAnalisis } from "../../components/layout/Analisis/AgregarAnalisis"
 export function VistaAnalisis() {
     return (
-        <div>Vista Analisis</div>
+        <section className="">
+            <TituloCustom titulo="Análisis" />
+            <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
+                <AgregarAnalisis titulo="Análisis Medico" />
+
+                <VistaCRUDAnalisis
+                    data={AnalisisData}
+                    titulo="pacientes"
+                    onEdit={(actualizado) => {
+                        console.log("Paciente actualizado:", actualizado);
+                    }}
+                />
+            </div>
+
+        </section>
     )
 }
