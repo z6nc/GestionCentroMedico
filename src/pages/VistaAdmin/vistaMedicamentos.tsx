c1ade504cfcfccaee720b397b5594cbe32985ea4
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-23 17:27:00 -0500

    Add VistaMedicamentos component for medication management with CRUD functionality

--- src/pages/VistaAdmin/vistaMedicamentos.tsx
+++ src/pages/VistaAdmin/vistaMedicamentos.tsx
@@ -1,5 +1,23 @@
+import { TituloCustom } from "../../components/common/titulos/tituloCustom"
+import { MedicamentosData } from "../../data/medicamentos.data"
+import { VistaCRUDMedicamentos } from "../../components/layout/Medicamentos/vistaCrudMedicamentos"
+import { AgregarMedicamentos } from "../../components/layout/Medicamentos/AgregarMedicamentos"
 export function VistaMedicamentos() {
     return (
-        <div>Vista Medicamentos</div>
+        <section className="">
+            <TituloCustom titulo="Medicamento" />
+            <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
+                <AgregarMedicamentos titulo="Medicamentos" />
+
+                <VistaCRUDMedicamentos
+                    data={MedicamentosData}
+                    titulo="Medicamentos"
+                    onEdit={(actualizado) => {
+                        console.log("Medicamento actualizado:", actualizado);
+                    }}
+                />
+            </div>
+
+        </section>
     )
 }
