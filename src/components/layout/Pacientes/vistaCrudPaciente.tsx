a7b27acca03804c25957baaae231f14da077246e
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-23 17:26:28 -0500

    Add VistaCRUDPaciente component for patient management with editing functionality

---
+++ src/components/layout/Pacientes/vistaCrudPaciente.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { TablaCustom } from "../../common/Tablas/tabla";
+import { ModalCustom } from "../../common/Modal/modalCustom";
+import { FormModalPaciente } from "./formPaciente";
+import type { Paciente } from "../../../schema/paciente.schema";
+
+interface VistaCRUDProps {
+  titulo: string;
+  data: Paciente[];
+  onEdit: (actualizado: Paciente) => void; // callback al editar
+}
+
+export function VistaCRUDPaciente({
+  titulo,
+  data,
+  onEdit,
+}: VistaCRUDProps) {
+  const [editItem, setEditItem] = useState<Partial<Paciente> | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Cuando se hace clic en el ícono de editar
+  const handleEditClick = (item: Paciente) => {
+    setEditItem(item);
+    setIsModalOpen(true);
+  };
+
+  // Cuando se envía el formulario dentro del modal
+  const handleSubmit = (data: Paciente) => {
+    onEdit(data); // llama al callback pasado desde el padre
+    setEditItem(null);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="flex flex-col gap-6 w-full">
+      <TablaCustom datas={data} onEdit={handleEditClick} />
+
+      {editItem && (
+        <ModalCustom
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title={`Editar ${titulo}`}
+        >
+          <FormModalPaciente
+            onSubmit={handleSubmit}
+            initialData={editItem}
+            mode="editar"
+          />
+        </ModalCustom>
+      )}
+    </div>
+  );
+}
