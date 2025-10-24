c1034340acda3389d2277708c05a17e1e881b7b4
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-23 21:01:53 -0500

    Refactor VistaMedicos component to display doctor list with AgregarMedico and VistaCRUDMedicos

--- src/pages/VistaAdmin/vistaMedicos.tsx
+++ src/pages/VistaAdmin/vistaMedicos.tsx
@@ -1,5 +1,22 @@
-export function VistaMedicos() {
+import { TituloCustom } from "../../components/common/titulos/tituloCustom"
+import { MedicosData } from "../../data/medicos.data"
+import { AgregarMedico } from "../../components/layout/Medicos/AgregarMedicos"
+import { VistaCRUDMedicos } from "../../components/layout/Medicos/vistaCrudMedicos"
+export function VistaMedico() {
     return (
-        <div>Vista Medicos</div>
+        <section className="">
+            <TituloCustom titulo="Lista de Medicos" />
+            <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
+                <AgregarMedico titulo="Medicos" />
+                <VistaCRUDMedicos
+                    data={MedicosData}
+                    titulo="Medicos"
+                    onEdit={(actualizado) => {
+                        console.log("Medico actualizado:", actualizado);
+                    }}
+                />
+            </div>
+
+        </section>
     )
 }
