a42af6fd5a1ed65718a5826327ca0c72c8a3d5f0
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-23 21:01:42 -0500

    Fix typo in import statement for VistaMedico in sidebarItems

--- src/data/itemsDataNavbar.tsx
+++ src/data/itemsDataNavbar.tsx
@@ -1,5 +1,5 @@
 import { Users, Calendar, Stethoscope, Pill, CalendarSearch } from "lucide-react"
-import { VistaCitas, VistaPacientes, VistaHistoriaMedica, VistaMedicos, VistaMedicamentos, VistaAnalisis } from "../pages/VistaAdmin/index"
+import { VistaCitas, VistaPacientes, VistaHistoriaMedica, VistaMedico, VistaMedicamentos, VistaAnalisis } from "../pages/VistaAdmin/index"
 import { VerBoletas } from "../pages/VistaCajero/index";
 import { VistaAtencionMedica } from "../pages/VistaMedico";
 import type { SidebarItem } from "../types/siderBarItem.types";
@@ -12,7 +12,7 @@ export const sidebarItems: SidebarItem[] = [
             { icon: Users, label: "Pacientes", path: "/dashboard/pacientes", element: <VistaPacientes />, allowedRoles: ["administrativo"] },
             { icon: Calendar, label: "Citas", path: "/dashboard/citas", element: <VistaCitas />, allowedRoles: ["administrativo"] },
             { icon: Calendar, label: "Historia Medica", path: "/dashboard/historia-medica", element: <VistaHistoriaMedica />, allowedRoles: ["administrativo"] },
-            { icon: Stethoscope, label: "Lista Medicos", path: "/dashboard/lista-medicos", element: <VistaMedicos />, allowedRoles: ["administrativo"] },
+            { icon: Stethoscope, label: "Lista Medicos", path: "/dashboard/lista-medicos", element: <VistaMedico />, allowedRoles: ["administrativo"] },
             { icon: Pill, label: "Medicamentos", path: "/dashboard/lista-medicamentos", element: <VistaMedicamentos />, allowedRoles: ["administrativo", "cajero"] },
             { icon: Pill, label: "Analisis", path: "/dashboard/analisis", element: <VistaAnalisis />, allowedRoles: ["administrativo"] },
 
