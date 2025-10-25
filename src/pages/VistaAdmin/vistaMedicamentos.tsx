be445126ded3cccd916f0641dedfeefd7ac8ec90
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-24 20:07:28 -0500

    Comment out unused imports and components in VistaMedicamentos for cleaner code

--- src/pages/VistaAdmin/vistaMedicamentos.tsx
+++ src/pages/VistaAdmin/vistaMedicamentos.tsx
@@ -1,11 +1,11 @@
-import { TituloCustom } from "../../components/common/titulos/tituloCustom"
-import { MedicamentosData } from "../../data/medicamentos.data"
-import { VistaCRUDMedicamentos } from "../../components/layout/Medicamentos/vistaCrudMedicamentos"
-import { AgregarMedicamentos } from "../../components/layout/Medicamentos/AgregarMedicamentos"
+// import { TituloCustom } from "../../components/common/titulos/tituloCustom"
+// import { MedicamentosData } from "../../data/medicamentos.data"
+// import { VistaCRUDMedicamentos } from "../../components/layout/Medicamentos/vistaCrudMedicamentos"
+// import { AgregarMedicamentos } from "../../components/layout/Medicamentos/AgregarMedicamentos"
 export function VistaMedicamentos() {
     return (
         <section className="">
-            <TituloCustom titulo="Medicamento" />
+            {/* <TituloCustom titulo="Medicamento" />
             <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
                 <AgregarMedicamentos titulo="Medicamentos" />
 
@@ -16,8 +16,8 @@ export function VistaMedicamentos() {
                         console.log("Medicamento actualizado:", actualizado);
                     }}
                 />
-            </div>
-
+            </div> */}
+            <h2>Vista de Medicamentos - En construcción</h2>
         </section>
     )
 }
