4d4f7e2e6a04d95829c1f090be9d218ca2489b90
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-24 20:07:32 -0500

    Comment out unused imports and components in VistaMedico for cleaner code

--- src/pages/VistaAdmin/vistaMedicos.tsx
+++ src/pages/VistaAdmin/vistaMedicos.tsx
@@ -1,11 +1,11 @@
-import { TituloCustom } from "../../components/common/titulos/tituloCustom"
-import { MedicosData } from "../../data/medicos.data"
-import { AgregarMedico } from "../../components/layout/Medicos/AgregarMedicos"
-import { VistaCRUDMedicos } from "../../components/layout/Medicos/vistaCrudMedicos"
+// import { TituloCustom } from "../../components/common/titulos/tituloCustom"
+// import { MedicosData } from "../../data/medicos.data"
+// import { AgregarMedico } from "../../components/layout/Medicos/AgregarMedicos"
+// import { VistaCRUDMedicos } from "../../components/layout/Medicos/vistaCrudMedicos"
 export function VistaMedico() {
     return (
         <section className="">
-            <TituloCustom titulo="Lista de Medicos" />
+            {/* <TituloCustom titulo="Lista de Medicos" />
             <div className=" xl:max-w-max m-9 flex flex-col gap-y-9 items-start ">
                 <AgregarMedico titulo="Medicos" />
                 <VistaCRUDMedicos
@@ -15,7 +15,8 @@ export function VistaMedico() {
                         console.log("Medico actualizado:", actualizado);
                     }}
                 />
-            </div>
+            </div> */}
+            <h2>Vista de Médicos - En construcción</h2>
 
         </section>
     )
