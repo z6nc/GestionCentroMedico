2f865e3d11fec83984aa2b97ba087ac090fee7b4
z6nc <andresvelasquezarotinco@gmail.com>
2025-10-27 13:39:04 -0500

    Refactor pacienteSchema to include optional blood type and ensure email validation

--- src/schema/paciente.schema.ts
+++ src/schema/paciente.schema.ts
@@ -8,12 +8,18 @@ export const pacienteSchema = z.object({
     Nombre: z.string().min(1, "Por favor ingresa el nombre del paciente"),
     Apellido: z.string().min(1, "Por favor ingresa el apellido del paciente"),
     FechaNacimiento: z.iso.date(),
+    Edad : z.string().min(1, "Por favor ingresa la edad del paciente"),
     Genero: z.enum(["Masculino", "Femenino"]),
     Telefono: z
         .string()
         .regex(/^9\d{8}$/, "El teléfono debe comenzar con 9 y tener 9 dígitos"),
     Direccion: z.string().optional(),
-    Email: z.email("Ingresa un email válido").optional(),
+    TipodeSangre: z
+        .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+        .optional(),
+    Email: z.email("Ingresa un email válido"),
+    PesoPaciente: z.string(),
+    AlturaPaciente: z.string(),
 });
 
 export type Paciente = z.infer<typeof pacienteSchema>;
