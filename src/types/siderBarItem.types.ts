cc94d9ed9ffb573880bd9abcf956f0753aeaed0e
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:41:25 -0500

    Refactor SidebarItem interface: remove icon and label properties from itemsLabel

--- src/types/siderBarItem.types.ts
+++ src/types/siderBarItem.types.ts
@@ -2,8 +2,6 @@ import type { Role } from "./roles.types";
 export interface SidebarItem {
     Categoria: Role;
     itemsLabel: {
-        icon: React.ComponentType<{ className?: string }>
-        label: string
         path: string
         allowedRoles?: Role[];
         element: React.ReactNode;
