ad1b6936d8abb3e0c094b97f393f0edbdd930b19
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:41:45 -0500

    Refactor App component: restructure JSX for better readability and maintainability

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,40 +5,55 @@ import Dashboard from "./dashboard";
 import { sidebarItems } from "../data/itemsDataNavbar";
 import { VistaPrincipal } from "../components/layout/Dasboard/vistaPrincipal";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<VistaLogin />} />
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000} // 3 segundos
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Routes>
+        <Route path="/" element={<VistaLogin />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProteccionRouter>
-            <Dashboard />
-          </ProteccionRouter>
-        }
-      >
-        <Route index element={<VistaPrincipal />} />
-        {/* Subrutas del dashboard */}
-        {sidebarItems.map(routes => (
-          routes.itemsLabel.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={
-                <ProteccionRouter allowedRoles={route.allowedRoles}>
-                  {route.element}
-                </ProteccionRouter>
-              }
-            />
-          ))
-        ))}
+        <Route
+          path="/dashboard"
+          element={
+            <ProteccionRouter>
+              <Dashboard />
+            </ProteccionRouter>
+          }
+        >
+          <Route index element={<VistaPrincipal />} />
+          {/* Subrutas del dashboard */}
+          {sidebarItems.map(routes => (
+            routes.itemsLabel.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <ProteccionRouter allowedRoles={route.allowedRoles}>
+                    {route.element}
+                  </ProteccionRouter>
+                }
+              />
+            ))
+          ))}
 
-      </Route>
-    </Routes>
-
+        </Route>
+      </Routes>
+      
+    </>
   )
 }
 
