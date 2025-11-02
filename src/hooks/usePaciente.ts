ebbb43287cbdb6f48dc7b8bcb78e8aecae707370
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:41:40 -0500

    Refactor usePacientes hook: update API endpoint and remove loading state calculation

--- src/hooks/usePaciente.ts
+++ src/hooks/usePaciente.ts
@@ -5,11 +5,10 @@ import type  { Paciente } from "../schema/paciente.schema";
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export function usePacientes() {
-  const { data, error, mutate } = useSWR<Paciente[]>("/api/pacientes", fetcher);
+  const { data, error, mutate } = useSWR<Paciente[]>("http://localhost:8092/paciente/listar", fetcher);
   return {
     pacientes: data,
     error,
-    isLoading: !data && !error,
     mutate, // permite actualizar la data localmente o volver a fetch
   };
 }
