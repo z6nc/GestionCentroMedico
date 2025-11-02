4bd936300b7e6f6495feb0bb797f1125562c7543
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:42:07 -0500

    Refactor paciente schema: standardize field names and improve validation rules

--- src/schema/paciente.schema.ts
+++ src/schema/paciente.schema.ts
@@ -1,25 +1,32 @@
 import { z } from "zod";
 
 export const pacienteSchema = z.object({
-    DniPaciente: z
+    numero: z.number().optional(),
+    dni: z
         .string()
         .min(8, "El DNI debe tener 8 dígitos")
         .max(8, "El DNI debe tener 8 dígitos"),
-    Nombre: z.string().min(1, "Por favor ingresa el nombre del paciente"),
-    Apellido: z.string().min(1, "Por favor ingresa el apellido del paciente"),
-    FechaNacimiento: z.iso.date(),
-    Edad : z.string().min(1, "Por favor ingresa la edad del paciente"),
-    Genero: z.enum(["Masculino", "Femenino"]),
-    Telefono: z
+    nombre: z.string().min(1, "Por favor ingresa el nombre del paciente"),
+    apellido: z.string().min(1, "Por favor ingresa el apellido del paciente"),
+    fechaNacimiento: z.iso.date(),
+    // Genero: z.enum(["Masculino", "Femenino"]),
+    email: z.email("Ingresa un email válido").optional().or(z.literal("")),
+    direccion: z.string().optional(),
+    telefono: z
         .string()
         .regex(/^9\d{8}$/, "El teléfono debe comenzar con 9 y tener 9 dígitos"),
-    Direccion: z.string().optional(),
-    TipodeSangre: z
-        .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
-        .optional(),
-    Email: z.email("Ingresa un email válido"),
-    PesoPaciente: z.string(),
-    AlturaPaciente: z.string(),
+    estado: z.preprocess(
+        (val) => val === "true",
+        z.boolean()
+    )
+
+    // ESTO ES PARA  HISTORIA MEDICA FUTURAMENTE
+    // Edad : z.string().min(1, "Por favor ingresa la edad del paciente"),
+    // TipodeSangre: z
+    //     .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+    //     .optional(),
+    // PesoPaciente: z.string(),
+    // AlturaPaciente: z.string(),
 });
 
 export type Paciente = z.infer<typeof pacienteSchema>;
