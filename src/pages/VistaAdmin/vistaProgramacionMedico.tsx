25e40dfe9cfcf1b410106675cfaf46889c48559f
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 20:50:54 -0500

    Refactor VistaProgramacionMedica: update component structure, enhance UI with TituloCustom and ModalCustom, and improve data handling

--- src/pages/VistaAdmin/vistaProgramacionMedico.tsx
+++ src/pages/VistaAdmin/vistaProgramacionMedico.tsx
@@ -1,68 +1,44 @@
 import { useParams } from "react-router-dom";
-import { FormHorario } from "../../components/layout/Medicos/DetalleMedico/formHorario";
+import { useState } from "react";
+import { FormHorario } from "../../components/layout/Medicos/DetalleProgramacionMedico/formHorario";
 import { useMedicosPorEspecialidad } from "../../hooks/useMedicos";
 import { useCarritoHorario } from "../../hooks/useCarritoMedico";
-import { CalendarioHorarios } from "../../components/layout/Medicos/DetalleMedico/calendario";
+import { CalendarioHorarios } from "../../components/layout/Medicos/DetalleProgramacionMedico/calendario";
+import { CarritoHorarios } from "../../components/layout/Medicos/DetalleProgramacionMedico/carritoHorario";
+import { TituloCustom } from "../../components/common/titulos/tituloCustom";
+import { DataTable } from "../../components/common/Tablas/tabla"
+import { columnasCarritoHorarios } from "../../components/layout/Medicos/DetalleProgramacionMedico/columnaProgramacionMedica";
+import { ModalCustom } from "../../components/common/Modal/modalCustom";
 import type { CarritoHorario } from "../../hooks/useCarritoMedico";
 export function VistaProgramacionMedica() {
+    const [CalendarioOpen, setCalendarioOpen] = useState(false);
     const { horarios, agregarHorario, quitarHorario } = useCarritoHorario();
     const { medicos } = useMedicosPorEspecialidad();
     const { medicoId } = useParams();
-
-
-
-    // En tu componente padre:
     const handleAddHorario = (horario: Omit<CarritoHorario, "medicoId" | "id" | "numero">) => {
         agregarHorario({ ...horario, medicoId: Number(medicoId) }); // agrega el ID del médico automáticamente
     };
+
     const medico = medicos?.find((m) => m.numero === Number(medicoId));
     const HorarioDemedico = horarios.filter((h) => h.medicoId === Number(medicoId));
 
     return (
-        <section className="p-6">
-            <h1 className="text-2xl font-bold mb-4">
-                Programación médica para:{" "}
-                {medico ? (
-                    <span className="text-blue-600">
-                        {medico.nombre} {medico.apellido}
-                    </span>
-                ) : (
-                    "Cargando..."
-                )}
-            </h1>
+        <section className=" flex  flex-col gap-y-9">
+            <TituloCustom titulo={`Programación médica para: ${medico?.nombre} ${medico?.apellido}`} />
 
-            <div className="flex gap-x-5 ">
+
+            <div className="flex gap-x-5  px-8 py-4">
                 <FormHorario medicoId={Number(medicoId)} onAdd={handleAddHorario} />
-
-                {/* Mostrar horarios agregados */}
-                <div className="mt-6 flex-1">
-                    <h2 className="font-semibold mb-2">Horarios agregadosss:</h2>
-                    <ul className="space-y-2">
-                        {HorarioDemedico.map((h) => (
-                            <li key={h.id} className="border p-2 rounded bg-gray-50 flex justify-between items-center">
-                                {medico?.nombre} {h.fecha} | {h.horaInicio} - {h.horaFin} | {h.consultorio}
-                                <button
-                                    onClick={() => quitarHorario(h.id!)}
-                                    className="ml-2 bg-red-500 text-white px-2 py-1 rounded"
-                                >
-                                    Quitar
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                    {/* <button
-                        onClick={handleAddHorario}
-                        className="mt-6 bg-blue-600 text-white px-4 py-2 rounded-lg"
-                    >
-                        Guardar Programaciónsss
-                    </button> */}
-                </div>
-
-
-     
+                <CarritoHorarios HorarioDemedico={HorarioDemedico} eliminarHorario={quitarHorario} />
             </div>
-            {/* <CalendarAdvanced horarios={HorarioDemedico} date="2025-10-20" /> */}
-            <CalendarioHorarios horarios={HorarioDemedico} />
+            <div className="px-8 py-4">
+                <button onClick={() => setCalendarioOpen(true)} className="btn">Ver Calendario de Horarios</button>
+                <DataTable columns={columnasCarritoHorarios()} data={HorarioDemedico} />
+            </div>
+                
+            <ModalCustom isOpen={CalendarioOpen} onClose={() => setCalendarioOpen(false)} title="Calendario de Horarios">
+             <CalendarioHorarios horarios={HorarioDemedico} />
+            </ModalCustom>
 
 
 
