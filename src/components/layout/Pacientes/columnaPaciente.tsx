d8473e8444dd594c21486b0463f92fcc673d0739
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:41:18 -0500

    Refactor columnasPacientes: update accessor names and improve action button styling

--- src/components/layout/Pacientes/columnaPaciente.tsx
+++ src/components/layout/Pacientes/columnaPaciente.tsx
@@ -1,35 +1,38 @@
 import type { Paciente } from "../../../schema/paciente.schema";
 import type { Column } from "../../common/Tablas/tabla";
 import { Link } from "react-router-dom";
+import { FileBadge, UserPen } from "lucide-react";
 
 export const columnasPacientes = (handleEditar: (p: Paciente) => void): Column<Paciente>[] => [
-    { header: "DNI", accessor: "DniPaciente" },
-    { header: "Nombre", accessor: "Nombre" },
-    { header: "Apellido", accessor: "Apellido" },
-    { header: "Género", accessor: "Genero" },
-    { header: "Fecha de Nacimiento", accessor: "FechaNacimiento" },
-    { header: "Edad", accessor: "Edad" },
-    { header: "Teléfono", accessor: "Telefono" },
-    { header: "Dirección", accessor: "Direccion" },
-    { header: "Email", accessor: "Email" },
-    { header: "Tipo de Sangre", accessor: "TipodeSangre" },
-    { header: "Peso", accessor: "PesoPaciente" },
-    { header: "Altura", accessor: "AlturaPaciente" },
+    {header : "Número", accessor: "numero" },
+    { header: "DNI", accessor: "dni" },
+    { header: "Nombre", accessor: "nombre" },
+    { header: "Apellido", accessor: "apellido" },
+    // { header: "Género", accessor: "Genero" },
+    { header: "Fecha de Nacimiento", accessor: "fechaNacimiento" },
+    // { header: "Edad", accessor: "Edad" },
+    { header: "Teléfono", accessor: "telefono" },
+    { header: "Dirección", accessor: "direccion" },
+    { header: "Email", accessor: "email" },
+    { header: "Estado", accessor: "estado"},
+    //{ header: "Tipo de Sangre", accessor: "TipodeSangre" },
+    //{ header: "Peso", accessor: "PesoPaciente" },
+    //{ header: "Altura", accessor: "AlturaPaciente" },
     {
         header: "Acciones",
         cell: (row: Paciente) => (
             <div className="flex flex-wrap gap-2">
               
-                <Link to={`/dashboard/historia-medica/${row.DniPaciente}`}>
-                    <button className="ml-2 px-3 py-1 text-blue-600 border border-blue-500 rounded-md hover:bg-blue-50 cursor-pointer">
-                        Historial
+                <Link to={`/dashboard/historia-medica/${row.numero}`}>
+                    <button className="ml-2 px-3 py-1 bg-blue-400 border text-white rounded-full cursor-pointer inline-flex items-center">
+                         <FileBadge />
                     </button>
                 </Link>
                 <button
                     onClick={() => handleEditar(row)}
-                    className="ml-2 px-3 py-1 text-green-600 border border-green-500 rounded-md hover:bg-green-50 cursor-pointer"
+                    className="ml-2 px-3 py-1 bg-orange-400 border text-white rounded-full cursor-pointer"
                 >
-                    Editar
+                    <UserPen />
                 </button>
             </div>
 
