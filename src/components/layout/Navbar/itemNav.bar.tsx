2e798671b0da96f848cfef7c28b2adbf91dacc53
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-01 23:41:12 -0500

    Refactor navigation component: update import and fix item mapping in ItemNavBar

--- src/components/layout/Navbar/itemNav.bar.tsx
+++ src/components/layout/Navbar/itemNav.bar.tsx
@@ -1,10 +1,10 @@
-import { sidebarItems } from "../../../data/itemsDataNavbar";
+import { ItemLabelNavbar } from "../../../data/itemsDataNavbar";
 import { Link, useLocation } from "react-router-dom";
 import type { Role } from "../../../types/roles.types";
 
 export const ItemNavBar = ({ tipo }: { tipo: Role }) => {
     const { pathname } = useLocation();
-    const FindSiderItems = sidebarItems.find((item) => item.Categoria === tipo);
+    const FindSiderItems = ItemLabelNavbar.find((item) => item.Categoria === tipo);
     const linkClass = (path: string) =>
         ` ${pathname === path ? "bg-white/20 " : "bg-transparent hover:bg-white/20"
         }`;
@@ -13,7 +13,7 @@ export const ItemNavBar = ({ tipo }: { tipo: Role }) => {
         <nav className="space-y-5 flex flex-col items-center text-white px-4">
             {
                 FindSiderItems ? (
-                    FindSiderItems.itemsLabel.map((item, index) => (
+                    FindSiderItems.items.map((item, index) => (
                         <Link to={item.path} className={`flex items-center gap-3 w-full px-4 py-2 rounded-lg  transition-colors text-left ${linkClass(item.path)}`}
                             key={index}
                         >
