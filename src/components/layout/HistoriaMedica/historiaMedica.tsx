2f040289c79ae44e6da048bd3402728844b7f049
z6nc <andresvelasquezarotinco@gmail.com>
2025-11-10 18:00:02 -0500

    Add modal for managing medical history: implement add/edit functionality with FormModalHistoriaMedica and integrate with patient data

--- src/components/layout/HistoriaMedica/historiaMedica.tsx
+++ src/components/layout/HistoriaMedica/historiaMedica.tsx
@@ -3,13 +3,43 @@ import type { Paciente } from "../../../schema/paciente.schema"
 import type { PropsListaAtencioMedica } from "../../../data/historiaMedica.data"
 import { useState } from "react"
 import { AppointmentCard } from "./AppointmentCard"
-interface HistoriaMedicaProps {
+import { FormModalHistoriaMedica } from "./formHistoriaMedica"
+import { ModalCustom } from "../../common/Modal/modalCustom"
+import type { HistoriaMedicaProps } from "../../../schema/historiaMedica.schema"
+interface HistoriaMedicaDataProps {
     pacienteProps: Paciente
-    historiaProps: PropsListaAtencioMedica[]
+    ListaAtencionProps: PropsListaAtencioMedica[]
+    historiaMedicaProps?: HistoriaMedicaProps
 }
 
-export function HistoriaMedicaPaciente({ pacienteProps, historiaProps }: HistoriaMedicaProps) {
+export function HistoriaMedicaPaciente({ pacienteProps, ListaAtencionProps, historiaMedicaProps }: HistoriaMedicaDataProps) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modo, setModo] = useState<'agregar' | 'editar'>('agregar');
     const [selectedId, setSelectedId] = useState<string | null>(null)
+    const [historiaSeleccionada, setHistoriaSeleccionada] = useState<HistoriaMedicaProps | null>(null);
+    const { edad, peso, altura, tipoSangre } = historiaMedicaProps || {};
+    const { nombre, apellido } = pacienteProps || {};
+    const datoscompletos = {
+        nombre,
+        apellido,
+        edad,
+        peso,
+        altura,
+        tipoSangre
+    };
+
+    const handleEditar = (historia: HistoriaMedicaProps) => {
+        setHistoriaSeleccionada(historia);
+        setModo('editar');
+        setIsModalOpen(true);
+    };
+
+    const handleAgregar = () => {
+        setHistoriaSeleccionada(null);
+        setModo('agregar');
+        setIsModalOpen(true);
+    };
+
 
     const handleClick = (id: string) => {
         // Si ya está abierto, lo cerramos
@@ -23,18 +53,36 @@ export function HistoriaMedicaPaciente({ pacienteProps, historiaProps }: Histori
 
     return (
         <section className="text-black  flex  p-9 gap-9">
-            <ProfileCard
-                title="Información del Paciente"
-                data={pacienteProps}
-                Nombre={pacienteProps.Nombre}
-                Apellido={pacienteProps.Apellido}
-            />
+            <div className="flex flex-col gap-y-7">
+                <ProfileCard
+                    title="Información del Paciente"
+                    data={datoscompletos || {}}
+                    Nombre={pacienteProps.nombre}
+                    Apellido={pacienteProps.apellido}
+                />
+                <div className="flex flex-wrap gap-3">
+                    <button
+                        onClick={handleAgregar}
+                        className="bg-blue-600 p-2 text-white rounded-lg text-sm cursor-pointer"
+                    >
+                        Agregar Historia Medica
+                    </button>
+                    <button
+                        onClick={() => handleEditar(historiaMedicaProps!)}
+                        className="bg-green-500 p-2 text-white rounded-lg text-sm cursor-pointer"
+                    >
+                        Editar Historia Medica
+                    </button>
+                </div>
+
+
+            </div>
 
             <div className="flex-1 bg-white rounded-lg shadow-md p-5">
                 {
-                    historiaProps.length > 0 ? (
+                    ListaAtencionProps.length > 0 ? (
                         <div className="flex flex-col gap-6">
-                            {historiaProps.map((historia) => (
+                            {ListaAtencionProps.map((historia) => (
                                 <AppointmentCard key={historia.IdHistoria} citaMedica={historia} isSelected={selectedId === historia.IdHistoria} onClick={() => handleClick(historia.IdHistoria)} />
                             ))}
                         </div>
@@ -46,6 +94,24 @@ export function HistoriaMedicaPaciente({ pacienteProps, historiaProps }: Histori
 
 
             </div>
+
+            {isModalOpen && (
+                <ModalCustom
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                    title={`${modo} Historia Medica`}
+                >
+                    <FormModalHistoriaMedica
+                        onSubmit={(historiaEditada) => {
+                            console.log(historiaEditada);
+                            setIsModalOpen(false); // cerrar modal
+                        }}
+                        initialData={historiaSeleccionada || undefined}
+                        mode={modo}
+                        pacienteId={pacienteProps.numero || 0} // importante para agregar
+                    />
+                </ModalCustom>
+            )}
 
         </section>
     )
